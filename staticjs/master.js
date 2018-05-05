6b466ed0a88c743a7f1d2033f439bf375a6f7a70
theGouge <thegouge42@gmail.com>
2018-05-04 18:24:46 -0700

    Added some cool animations and cleaned up the design a bit

--- staticjs/master.js
+++ staticjs/master.js
@@ -1,7 +1,7 @@
 window.onload = function () {
     var header = document.getElementById('top');
     var footer = document.getElementById('bot');
-    var desiredHeight = window.innerHeight - header.offsetHeight - footer.offsetHeight;
+    var desiredHeight = window.innerHeight - header.offsetHeight - footer.offsetHeight - 30;
 
     document.getElementById('page').style.height = desiredHeight + "px";
 };
@@ -11,5 +11,5 @@ document.getElementById('header-text').addEventListener("mouseenter", function (
 });
 
 document.getElementById('header-text').addEventListener("mouseleave", function (event) {
-    document.getElementById('top').style.background = "rgb(161, 161, 161)";
+    document.getElementById('top').style.background = "rgb(100, 100, 100)";
 });
