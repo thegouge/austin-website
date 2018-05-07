0b5524eecd701dec885819d3a88f32f194fb8937
theGouge <thegouge42@gmail.com>
2018-05-07 11:29:11 -0700

    Updated the index page  for mobile handling

--- staticjs/master.js
+++ staticjs/master.js
@@ -1,10 +1,10 @@
-window.onload = function () {
-    var header = document.getElementById('top');
-    var footer = document.getElementById('bot');
-    var desiredHeight = window.innerHeight - header.offsetHeight - footer.offsetHeight - 30;
+// window.onload = function () {
+//     var header = document.getElementById('top');
+//     var footer = document.getElementById('bot');
+//     var desiredHeight = window.innerHeight - header.offsetHeight - footer.offsetHeight - 30;
 
-    document.getElementById('page').style.height = desiredHeight + "px";
-};
+//     document.getElementById('page').style.height = desiredHeight + "px";
+// };
 
 document.getElementById('header-text').addEventListener("mouseenter", function (event) {
     document.getElementById('top').style.background = "white";
